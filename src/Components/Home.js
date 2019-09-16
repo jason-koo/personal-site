0f8a50c2c969a2f616db1e17ea4b2d9dc17c1852
Jason Koo <jkoo2088@gmail.com>
2019-09-15 20:07:27 -0400

    updated file structure and completed Home component

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Home.css";
+class Home extends Component {
+  render () {
+    return (
+        <div>
+            <div className="main">
+                <div className="intro">Hello, I'm Jason!</div>
+                <div className="tagline">Student | Code Fanatic | Linux Hacker | Petrolhead</div>
+            </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
